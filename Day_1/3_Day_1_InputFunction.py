fc25e385906ec2cec6b3d3f6c1b9d5b5d2cb306b
Nikhil-r21 <rnikhilvignesh21@gmail.com>
2024-07-31 13:25:34 +0530

    Completed Day 1

---
+++ Day_1/3_Day_1_InputFunction.py
@@ -0,0 +1,24 @@
+print(len(input()))
+
+
+numOfLetters = len("Angela")
+print(numOfLetters)
+
+
+print("Hello there! "+ input())
+
+
+num1 = int(input())
+num2 = int(input())
+
+print(num1 * num2)
+
+# Input
+# 1 Jane
+# 2 Ben
+# 3 Tom
+
+
+print(input())
+print(input())
+print(input())
