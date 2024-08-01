744c8286b1a4d3271a1c37c67e3e0d82bf83c875
Nikhil-r21 <rnikhilvignesh21@gmail.com>
2024-08-01 16:16:48 +0530

    Day Pizza Oder exercise completed

---
+++ Day_3/11_Day_3_PizzaOrder.py
@@ -0,0 +1,51 @@
+# Instructions
+# Congratulations, you've got a job at Python Pizza! Your first job is to build an automatic pizza order program.
+
+# Based on a user's order, work out their final bill.
+
+# Small pizza (S): $15
+
+# Medium pizza (M): $20
+
+# Large pizza (L): $25
+
+# Add pepperoni for small pizza (Y or N): +$2
+
+# Add pepperoni for medium or large pizza (Y or N): +$3
+
+# Add extra cheese for any size pizza (Y or N): +$1
+
+# Example Input
+# L
+# Y
+# N
+# Example Output
+# Thank you for choosing Python Pizza Deliveries!
+# Your final bill is: $28.
+
+
+
+
+print("Thank you for choosing Python Pizza Deliveries!")
+size = input()
+add_pepperoni = input() 
+extra_cheese = input() 
+bill = 0
+
+if size == "S":
+  bill += 15
+elif size == "M":
+  bill += 20
+else:
+  bill += 25
+
+if add_pepperoni == "Y":
+  if size == "S":
+    bill += 2
+  else:
+    bill += 3
+
+if extra_cheese == "Y":
+  bill += 1
+
+print(f"Your final bill is: ${bill}.")
