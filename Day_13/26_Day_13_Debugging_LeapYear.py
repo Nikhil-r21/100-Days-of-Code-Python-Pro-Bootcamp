0b68021c7548c7d6f7d622d852fefbb5db569794
Nikhil-r21 <rnikhilvignesh21@gmail.com>
2024-08-07 10:01:44 +0530

    Debugging leap year

---
+++ Day_13/26_Day_13_Debugging_LeapYear.py
@@ -0,0 +1,30 @@
+# Read this the code in main.py
+# Spot the problems.
+# Modify the code to fix the program.
+# No shortcuts - don't copy-paste to replace the code entirely with a working solution.
+# Fix the code so that it works and when you hit submit it should pass all the tests.
+
+
+
+
+# SOLUTION
+
+
+
+
+year = int(input()) # TypeError without int() conversion
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
+
+
+
+  
